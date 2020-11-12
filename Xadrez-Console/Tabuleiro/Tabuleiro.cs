3782ea57c8f31e042234e4d9dfe56b30895b9d75
erineia <erineiavs@gmail.com>
2020-11-12 16:59:24 -0300

    Começando a implementar o tabuleiro

---
+++ Xadrez-Console/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        public Peca[,] pecas;
+
+        public Tabuleiro(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+            pecas = new Peca[linha, coluna];
+        }
+    }
+}
